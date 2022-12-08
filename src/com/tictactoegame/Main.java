cea1e0bc163186bc19d13ddd86b250cca5589f75
Prashik Kamble <kambleprashik7349@gmail.com>
2022-12-08 22:03:37 +0530

    ADDED : Code to tic tac toe game board.

--- src/com/tictactoegame/Main.java
+++ src/com/tictactoegame/Main.java
@@ -2,6 +2,8 @@ package com.tictactoegame;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Tic_Tac_Toe Game!");
+        System.out.println("Welcome to Tic Tac Toe!");
+        TicTacToeGame ticTacToeGame = new TicTacToeGame();
+        ticTacToeGame.creatingBoard();
     }
 }
---
+++ src/com/tictactoegame/TicTacToeGame.java
@@ -0,0 +1,17 @@
+package com.tictactoegame;
+
+public class TicTacToeGame {
+    public static char[] board = new char[10];
+
+    public void creatingBoard() {
+        for (int i = 0; i < board.length; i++) {
+            board[i] = ' ';
+        }
+        System.out.println(" " + board[0] + " | " + board[1] + " | " + board[2] + " ");
+        System.out.println("-----------");
+        System.out.println(" " + board[3] + " | " + board[4] + " | " + board[5] + " ");
+        System.out.println("-----------");
+        System.out.println(" " + board[6] + " | " + board[7] + " | " + board[8] + " ");
+
+    }
+}
